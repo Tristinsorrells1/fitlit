f575f5365a961e60918a409707a4964e59cf4ac4
Sarah Hampton <stoluba@gmail.com>
2022-12-12 15:35:08 -0500

    Change text color

--- src/scripts.js
+++ src/scripts.js
@@ -28,7 +28,7 @@ let sleepQualityAverage = document.querySelector("#sleepQualityAverage");
 let friendSection = document.querySelector(".friends-list");
 const stepsChart = document.getElementById("stepsChart").getContext("2d");
 const waterChart = document.getElementById("waterChart").getContext("2d");
-Chart.defaults.color = "rgb(219, 208, 208)";
+Chart.defaults.color = "#bdc1c6";
 Chart.defaults.font.size = 16;
 const sleepChart = document.getElementById("sleepChart").getContext("2d");
 
